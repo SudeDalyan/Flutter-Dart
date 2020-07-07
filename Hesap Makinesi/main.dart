1ecbd565ae292c3b27cdd4f98a6bfcbbf8ef1f38
Sude <64406311+SudeDalyan@users.noreply.github.com>
2020-07-07 14:59:58 +0300

    Add files via upload

---
+++ Hesap Makinesi/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  int x = 9;
+  int y = 7;
+  int toplama= x+y;
+  int cikarma= x-y;
+  int carpma= x*y;
+  int bolme= x ~/ y;
+  print("Birinci sayımız: $x ");
+  print("İkinci sayımız: $y ");
+  
+  print("Toplama: $x + $y = $toplama");
+  print("Çıkarma: $x - $y = $cikarma");
+  print("Çarpma: $x x $y = $carpma");
+  print("Bölme: $x / $y = $bolme");
+  
+  
+}
